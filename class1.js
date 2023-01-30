763a90ab382ac8cabf51348a8417f466e1cd9f05
Olamiposi Cloud <Olamiposi-cloud-coder>
2023-01-30 13:33:14 +0100

    objects classwork

--- class1.js
+++ class1.js
@@ -5,3 +5,16 @@ obj.name = "Pete"
 
 delete obj.name
 
+
+function multiplyNums(obj) {
+  for (let x in obj) {
+    if (typeof obj[x] === 'number') {
+      obj[x] *= 2;
+    }
+  }
+  return obj;
+}
+
+
+
+
